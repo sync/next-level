02acd5c77301f57f27c436134aff297d85924248
Anthony Mittaz <sync@me.com>
2019-07-09 22:24:52 +1000

    Setup tailwind and rework index page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,29 +27,6 @@ export default class MyDocument extends Document {
               display: flex;
               flex-direction: column;
             }
-
-            :root {
-              --navbar: #673AB7;
-              --navbarTextColor: #fff;
-              --navbarHeight: 56px;
-              --toolbarHeight: 44px;
-            }
-
-            * {
-              box-sizing: border-box !important;
-            }
-            
-            html {
-              font-size: 10px;
-            }
-            
-            body {
-              font-size: 1.6rem;
-              margin: 0;
-              font-family: system-ui, -apple-system, BlinkMacSystemFont, segoeUI, Roboto,
-                Ubuntu, 'Helvetica Neue', sans-serif;
-              -webkit-tap-highlight-color: transparent;
-            }
           `,
         }}
       />,
