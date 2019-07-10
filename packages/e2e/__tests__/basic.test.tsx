f4776dea68038d71e6bce16e5b0510e00b20776f
Anthony Mittaz <sync@me.com>
2019-07-10 11:26:00 +1000

    Add welcome api

--- packages/e2e/__tests__/basic.test.tsx
+++ packages/e2e/__tests__/basic.test.tsx
@@ -1,19 +1,25 @@
+import fetch from 'isomorphic-fetch';
 import config from '../jest-puppeteer.config';
 
 const timeout = 300000;
 
-const openPage = (pageUrl = '/') =>
-  global.page.goto(`http://localhost:${config.server.port}${pageUrl}`);
+const url = `http://localhost:${config.server.port}`;
+
+const openPage = (pageUrl = '/') => global.page.goto(`${url}${pageUrl}`);
 
 describe('Basic integration', () => {
+  let welcome;
+
   beforeEach(async () => {
+    const response = await fetch(`${url}/api/welcome`);
+    welcome = await response.json();
     await openPage();
   }, timeout);
 
   it(
     'displays a welcome text',
     async () => {
-      await expect(global.page).toMatch('Hello world!');
+      await expect(global.page).toMatch(`Hello ${welcome.name}!`);
     },
     timeout,
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import { NextPage } from 'next';
+import fetch from 'isomorphic-fetch';
 import Layout from '../src/components/Layout';
 
-const Page: NextPage = () => (
+type Props = {
+  welcome: {
+    name: string;
+  };
+};
+
+const Index: NextPage<Props> = ({ welcome }) => (
   <Layout>
     <h1 className="text-5xl font-bold text-purple-900 flex items-center justify-center h-screen w-screen bg-gray-200">
-      Hello world!
+      {`Hello ${welcome.name}!`}
     </h1>
   </Layout>
 );
 
-export default Page;
+Index.getInitialProps = async ({ req }) => {
+  function getBaseUrl(req) {
+    const protocol = req.headers['x-forwarded-proto'] || 'http';
+    const host = req.headers['x-forwarded-host'] || req.headers.host;
+    return `${protocol}://${host}/api`;
+  }
+
+  const baseUrl = req ? getBaseUrl(req) : '/api';
+  const welcome = await fetch(`${baseUrl}/welcome`).then(r => r.json());
+
+  return {
+    welcome,
+  };
+};
+
+export default Index;
